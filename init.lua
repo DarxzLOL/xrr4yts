90bc6d3849deaba97545386fcf9581ed8c70395e
DarxzLOL <brainwhy999@gmail.com>
2026-06-22 17:41:25 +0700

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,32 @@
+local Lib = {}
+
+Lib.Theme = require(script.Theme)
+Lib.Utility = require(script.Utility)
+Lib.Window = require(script.Window)
+Lib.Tab = require(script.Tab)
+Lib.Button = require(script.Button)
+Lib.Toggle = require(script.Toggle)
+Lib.Slider = require(script.Slider)
+Lib.Dropdown = require(script.Dropdown)
+Lib.Textbox = require(script.Textbox)
+Lib.Label = require(script.Label)
+Lib.Paragraph = require(script.Paragraph)
+Lib.Keybind = require(script.Keybind)
+Lib.Notification = require(script.Notification)
+Lib.Mobile = require(script.Mobile)
+
+local Lib = {}
+
+Lib.Window = require(script.Window)
+
+function Lib:Init()
+    local Window = self.Window:CreateWindow({
+        Title = "XRR4YTS Hub",
+        Name = "Main"
+    })
+
+    self._Window = Window
+    return Window
+end
+
+return Lib
