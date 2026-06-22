17164f1b0f9c5a2bd4eb1a8821919b67ac8a2bed
DarxzLOL <brainwhy999@gmail.com>
2026-06-22 17:30:15 +0700

    Create Mobile.lua

---
+++ Mobile.lua
@@ -0,0 +1,19 @@
+local Mobile = {}
+
+function Mobile:Enable(Window)
+    -- Mobile optimization
+end
+
+function Mobile:SetScale(Window)
+    -- Auto scale UI
+end
+
+function Mobile:EnableTouchDrag(Window)
+    -- Touch drag
+end
+
+function Mobile:EnableSafeArea(Window)
+    -- Safe area (notch)
+end
+
+return Mobile
