f1e8c1b8c8df99202d2e3f7a808704096b7f0401
DarxzLOL <brainwhy999@gmail.com>
2026-06-22 17:02:43 +0700

    Create Theme.lua

---
+++ Theme.lua
@@ -0,0 +1,8 @@
+local Theme = {}
+
+Theme.Background = Color3.fromRGB(22,22,22)
+Theme.Secondary = Color3.fromRGB(30,30,30)
+Theme.Accent = Color3.fromRGB(0,170,255)
+Theme.Text = Color3.fromRGB(255,255,255)
+
+return Theme
