a9b66868f106c3294fe86a28d65d618e3c431bb8
DarxzLOL <brainwhy999@gmail.com>
2026-06-22 17:34:47 +0700

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,18 @@
+local Lib = {}
+
+Lib.Theme = require(script.Theme)
+Lib.Utility = require(script.Utility)
+Lib.Window = require(script.Window)
+Lib.Tab = require(script.Tab)
+Lib.Button = require(script.Button)
+Lib.Toggle = require(script.Toggle)
+Lib.Slider = require(script.Slider)
+Lib.Dropdown = require(script.Dropdown)
+Lib.Textbox = require(script.Textbox)
+Lib.Label = require(script.Label)
+Lib.Paragraph = require(script.Paragraph)
+Lib.Keybind = require(script.Keybind)
+Lib.Notification = require(script.Notification)
+Lib.Mobile = require(script.Mobile)
+
+return Lib
